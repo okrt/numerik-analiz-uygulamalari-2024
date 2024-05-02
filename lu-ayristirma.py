73fe4fdc916b8b107c2f9e29a21129ed25d3db26
Oğuz Kırat <okrt@users.noreply.github.com>
2024-05-03 00:49:37 +0300

    Teams'e taşındı

--- lu-ayristirma.py
+++
@@ -1,8 +0,0 @@
-import scipy
-import scipy.linalg   
-A = scipy.array([ [7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6] ])
-P, L, U = scipy.linalg.lu(A)
-print("A:")
-print(P)
-print(L)
-print(U)
